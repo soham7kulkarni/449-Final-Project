5cdc5f811af68c007c2b44376cebcae6c1fd2cc9
Sanket Shahane <shahanesanket24@gmail.com>
2023-05-17 14:08:49 -0700

    main file changes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Importing all dependencies
+import uvicorn
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel 
@@ -27,8 +28,9 @@ except Exception as e:
 
 # Customized Pydantic model for data validation 
 class Book(BaseModel):
+    book_id:int
     title: str
-    author: str
+    author: Optional[str]
     description: Optional[str] = None
     price: float
     stock: int
@@ -44,8 +46,8 @@ async def root():
     return {"message": "Hello World"}
 
 @app.get("/books/{book_id}")
-async def get_book(book_id: str):
-    book = await collection.find_one({"_id": ObjectId(book_id)})
+async def get_book(book_id: int):
+    book = await collection.find_one({"book_id":book_id})
     return Book(**book)
 
 @app.get("/bookDetails")
@@ -63,15 +65,27 @@ async def add_book(book:Book):
     inserted_book = await collection.find_one({"_id": result.inserted_id})
     return Book(**inserted_book)
 
+@app.put("/books/{book_id}")
+async def update_book(book_id:int,book:Book):
+    result = await collection.find_one({"book_id":book_id})
+    if result:
+        updated_result=await collection.replace_one({"book_id":book_id},{"book_id":book_id,"title":book.title,"author":book.author,"description":book.description,"price":book.price,"stock":book.stock})
+        return {"message":f"total {updated_result.modified_count} books changed"}
+    else:
+        return "book with id : {book_id} not found"
+
 @app.delete("/{id}")
-async def delete_book(id: str):
-    result = await collection.find_one({"_id": ObjectId(id)})
+async def delete_book(id: int):
+    result = await collection.find_one({"book_id": id})
     if result:
         title = result['title']
-        await collection.delete_one({"_id": ObjectId(id)})
+        await collection.delete_one({"book_id": id})
         return {"message":f"book with title '{title}' deleted"}
     else:
         return {"message": f"Book with id '{id}' not found."}
+
+if __name__=="__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000)
  
 # @app.put("/books/{book_id}")
 # async def update_book():
